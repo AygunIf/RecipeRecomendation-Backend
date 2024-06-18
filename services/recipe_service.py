1976105d6cfdd82f7cd866769557ac375cd6c8b6
Juan Ignacio Zambrano <juano.zambrano@gmail.com>
2024-06-18 11:33:07 +0200

    recipes by dosha fixed

--- services/recipe_service.py
+++ services/recipe_service.py
@@ -1,6 +1,5 @@
 from model import Recipe, db
 from flask import jsonify
-from utils import filter_recipes_by_dosha
 from flask import current_app as app  # Import app from Flask context
 
 
@@ -16,14 +15,33 @@ def get_recipe_by_id(id):
         return {'message': 'Recipe not found.'}, 404
     return recep.to_dict(), 200
 
-def get_recipe_by_dosha_type(dosha_type):
-    
-    recipes = Recipe.query.all()
-    app.logger.info('Hello. Processing default request')
 
+def get_recipe_by_dosha_type(dosha_type):
+
+    if dosha_type == 'vata':
+        
+        recipes = Recipe.query.filter((Recipe.vata_dosha_score>0) & 
+                                  (Recipe.vata_dosha_score>Recipe.pitta_dosha_score) &
+                                  (Recipe.vata_dosha_score>Recipe.kapha_dosha_score)
+                                  ).order_by(Recipe.vata_dosha_score.desc()).limit(20).all()
+    elif dosha_type == 'pitta':
+
+        recipes = Recipe.query.filter((Recipe.pitta_dosha_score>0) & 
+                                  (Recipe.pitta_dosha_score>Recipe.vata_dosha_score) &
+                                  (Recipe.pitta_dosha_score>Recipe.kapha_dosha_score)
+                                  ).order_by(Recipe.pitta_dosha_score.desc()).limit(20).all()
+    elif dosha_type == 'kapha':
+
+        recipes = Recipe.query.filter((Recipe.kapha_dosha_score>0) & 
+                                  (Recipe.kapha_dosha_score>Recipe.pitta_dosha_score) &
+                                  (Recipe.kapha_dosha_score>Recipe.vata_dosha_score)
+                                  ).order_by(Recipe.kapha_dosha_score.desc()).limit(20).all()
+    else:
+
+         return {'message': "Invalid dosha type. Choose from 'vata', 'pitta', or 'kapha'."}, 405
+
+    
+    
     recipes_list = [recipe.to_dict() for recipe in recipes]
 
-    filtered_recipes = filter_recipes_by_dosha(recipes_list, dosha_type)
-    if filtered_recipes is None:
-         return {'message': 'No recipes for provided dosha type.'}, 404
-    return filtered_recipes, 200
+    return recipes_list, 200
--- utils.py
+++ utils.py
@@ -18,33 +18,3 @@ def require_api_key(f):
 def check_api_key(api_key):
     FOOD_API_KEY = "GkhP0QwCUZjNSCT2qq4pAQSqodp6iVGB"
     return api_key == FOOD_API_KEY
-
-
-def filter_recipes_by_dosha(recipes_list, dosha_type):
-    filtered_recipes = []
-    app.logger.info('Processing request for dosha type: %s', dosha_type)
-
-    for recipe in recipes_list:
-        if dosha_type == 'vata':
-            if (recipe['vata_dosha_score'] > 0 and 
-                recipe['vata_dosha_score'] > recipe['pitta_dosha_score'] and
-                recipe['vata_dosha_score'] > recipe['kapha_dosha_score']):
-                filtered_recipes.append(recipe)
-        elif dosha_type == 'pitta':
-            if (recipe['pitta_dosha_score'] > 0 and 
-                recipe['pitta_dosha_score'] > recipe['vata_dosha_score'] and
-                recipe['pitta_dosha_score'] > recipe['kapha_dosha_score']):
-                filtered_recipes.append(recipe)
-        elif dosha_type == 'kapha':
-            if (recipe['kapha_dosha_score'] > 0 and 
-                recipe['kapha_dosha_score'] > recipe['vata_dosha_score'] and
-                recipe['kapha_dosha_score'] > recipe['pitta_dosha_score']):
-                filtered_recipes.append(recipe)
-        else:
-            raise ValueError("Invalid dosha type. Choose from 'vata', 'pitta', or 'kapha'.")
-    
-    # Sort filtered recipes by respective dosha score and get top 20
-    sorted_recipes = sorted(filtered_recipes, key=lambda x: x[f"{dosha_type}_dosha_score"], reverse=True)
-    top_20_recipes = sorted_recipes[:20]
-
-    return top_20_recipes
